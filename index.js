7e9f3f1355eb263bb399b62af855468f3a882ba8
ebarlock20 <ebarlock20@gmail.com>
2025-12-19 13:27:41 -0500

    Updated small spelling error

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function refeshWeather(response) {
+function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
